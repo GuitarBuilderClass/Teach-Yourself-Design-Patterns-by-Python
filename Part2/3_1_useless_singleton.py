53337b4a2455fdcf15ccd96dbeca542fc8e17566
GuitarBuilderClass <wordsword.mk+git@gmail.com>
2018-12-14 19:52:04 +0900

    UPDATE: ch02 1 Singleton パターン適用前

--- Part2/3_1_useless_singleton.py
+++ Part2/3_1_useless_singleton.py
@@ -1,56 +1,55 @@
 #! /usr/bin/env python3
 
-HOURS: int = 24
-BOOKING: list = []
-for i in range(HOURS):
-    BOOKING += " "
+
+class CourtScheduler:
+    HOURS: int = 24
+
+    def __init__(self) -> None:
+        self.__initialize()
+
+    @classmethod
+    def __initialize(cls) -> list:
+        cls.booking: list = list()
+        for _ in range(cls.HOURS):
+            cls.booking += " "
+        return cls.booking
+
+    @classmethod
+    def schedule(cls) -> list:
+        return cls.booking
+
+    @classmethod
+    def schedule(cls, hour: int, person: str) -> None:
+        cls.booking[hour] = person
 
 
-# パターン未適用の場合
-class Scheduler:
-    """予約管理クラス"""
-
-    def __init__(self):
-        self.hours = HOURS
-
-    @staticmethod
-    def schedule():
-        return BOOKING
-
-    @staticmethod
-    def schedule(hour: int, person: str):
-        BOOKING[hour - 1] = person
-
-
-class Receptionist(Scheduler):
-    """予約受付クラス"""
-
-    # 予約開始-終了時間
+class Receptionist:
     START: int = 9
     END: int = 20
 
-    def __init__(self, name: str):
-        super(Receptionist, self).__init__()
+    def __init__(self, name: str) -> None:
         self.name: str = name
-        self.scheduler = Scheduler()
+        self.court_scheduler = CourtScheduler
+        self.hours: int = self.court_scheduler.HOURS
 
-    def make_reservation(self, hour: int, person: str):
-        if (hour < 0) or (hour > self.scheduler.hours-1):
+    def make_reservation(self, hour: int, person: str) -> None:
+        if (hour <= 0) or (hour > self.hours - 1):
             message = "指定時間が間違っています"
         elif (self.END < hour) or (hour < self.START):
             message = "営業時間外です"
         else:
-            if BOOKING[hour-1] == " ":
-                self.scheduler.schedule(hour, person)
+            if self.court_scheduler.booking[hour] == " ":
+                self.court_scheduler.schedule(hour, person)
                 message = f"{self.name}が{hour}時に{person}さんの予約を入れました"
             else:
-                message = f"{hour}時は既に予約が入っております\nあぁっと{person}君ふっ飛ばされた!!"
+                person2 = self.court_scheduler.booking[hour]
+                message = f"{hour}時は既に{person2}予約が入っております\nあぁっと{person}君ふっ飛ばされた!!"
         print(message)
 
 
 class Main:
     """メイン処理クラス"""
-    scheduler = Scheduler()
+    court_scheduler: CourtScheduler = CourtScheduler()
     receptionistA = Receptionist("A")
     receptionistB = Receptionist("B")
 
@@ -62,5 +61,5 @@ class Main:
     receptionistB.make_reservation(12, "森崎")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     Main()
