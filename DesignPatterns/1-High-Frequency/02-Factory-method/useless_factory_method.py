6a674356ee2ec4674b07b970d78b81924533f93c
GuitarBuilderClass <wordsword.mk+git@gmail.com>
2018-12-20 01:27:09 +0900

    Feat: Factory method 未適用時

---
+++ DesignPatterns/1-High-Frequency/02-Factory-method/useless_factory_method.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+
+FILE = "test.md"
+
+
+class FileDataObject:
+
+    def __init__(self):
+        self.data_list = list()
+
+        with open(FILE, 'r') as f:
+            for line in f:
+                self.data_list.append(line)
+
+    def read_data_object(self, row_num):
+        return self.data_list[row_num]
+
+
+class DbDataObject:
+    """未実装クラスだが、将来的に FileDataObject と差し替える予定"""
+
+    def __init__(self):
+        # DBへの接続手続きなど
+        pass
+
+    def read_data_object(self, row_num):
+        pass
+
+
+class Client:
+    def __init__(self):
+        self.data_object = FileDataObject()
+
+    def operating(self, row):
+        person = self.data_object.read_data_object(row)
+        return person
+
+
+if __name__ == '__main__':
+    client = Client()
+
+    i = 0
+    while True:
+        try:
+            print(client.operating(i))
+            i += 1
+        except IndexError:
+            break
