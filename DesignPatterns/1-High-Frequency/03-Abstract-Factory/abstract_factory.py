84075a777da7416ddadce3f3f9596c5df3f0c1a0
GuitarBuilderClass <wordsword.mk+git@gmail.com>
2019-02-10 00:27:54 +0900

    Add: Abstract Factory 適用メソッド名追加
    3種類を定義しているが、新たに種類を追加するのは困難

--- DesignPatterns/1-High-Frequency/03-Abstract-Factory/abstract_factory.py
+++ DesignPatterns/1-High-Frequency/03-Abstract-Factory/abstract_factory.py
@@ -2,6 +2,7 @@
 from enum import Enum, auto
 from typing import Union
 
+
 class GuitarType(Enum):
     Telecaster: int = auto()
     LesPaul: int = auto()
@@ -59,19 +60,73 @@ class LesPaulBody(Body):
         return "Mahogany and Maple"
 
 
-class GuitarFactory(Neck, Fingerboard, Body):
+class GuitarFactory:
 
-    neck: Union[TelecasterNeck, LesPaulNeck]
-    fingerboard: Union[TelecasterFingerboard, LesPaulFingerboard]
-    body: Union[TelecasterBody, LesPaulBody]
+    def choice_neck(self) -> Union[TelecasterNeck, LesPaulNeck]:
+        pass
 
-    neck = Neck.feature()
-    fingerbord = Fingerboard.feature()
-    body = Body.feature()
+    def choice_fingerboard(
+            self) -> Union[TelecasterFingerboard, LesPaulFingerboard]:
+        pass
 
-    def create_neck(self) -> str:
-
-
+    def choice_body(self) -> Union[TelecasterBody, LesPaulBody]:
+        pass
 
 
+class TelecasterFactory(GuitarFactory):
 
+    def choice_neck(self) -> TelecasterNeck:
+        return TelecasterNeck()
+
+    def choice_fingerboard(self) -> TelecasterFingerboard:
+        return TelecasterFingerboard()
+
+    def choice_body(self) -> TelecasterBody:
+        return TelecasterBody()
+
+
+class LesPaulFactory(GuitarFactory):
+
+    def choice_neck(self) -> LesPaulNeck:
+        return LesPaulNeck()
+
+    def choice_fingerboard(self) -> LesPaulFingerboard:
+        return LesPaulFingerboard()
+
+    def choice_body(self) -> LesPaulBody:
+        return LesPaulBody()
+
+
+class Order(GuitarFactory):
+
+    def __init__(self, factory):
+        factory: Union[TelecasterFactory, LesPaulFactory]
+
+        if factory == GuitarType.Telecaster:
+            factory = TelecasterFactory()
+        elif factory == GuitarType.LesPaul:
+            factory = LesPaulFactory()
+
+        self.neck: Union[TelecasterFactory, LesPaulFactory]
+        self.fingerboard: Union[TelecasterFactory, LesPaulFactory]
+        self.body: Union[TelecasterFactory, LesPaulFactory]
+
+        self.neck = factory.choice_neck()
+        self.fingerboard = factory.choice_fingerboard()
+        self.body = factory.choice_body()
+
+    def make_guitar(self):
+        return self.neck, self.fingerboard, self.body
+
+
+if __name__ == '__main__':
+    # ギターの種類を Order クラスの責任で指定
+    guitar = GuitarType.Telecaster
+    order = Order(guitar)
+
+    neck = order.neck.feature()
+    fingerboard = order.fingerboard.feature()
+    body = order.body.feature()
+    print(f"{guitar.name}のネックは{neck}、" +
+          f"フィンガーボードは{fingerboard}、" +
+          f"ボディは{body}でできています。")
